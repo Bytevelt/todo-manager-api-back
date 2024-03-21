ea86c537016997ef6619ec91555dbe0f7561b49a
Bytevelt <rooseveltdelagarza.dev@gmail.com>
2024-03-20 23:56:57 -0600

    Add AppDbContext

---
+++ TodoManager/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using TodoManager.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace TodoManager.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        { 
+        
+        }
+        public DbSet<NewTask> Tasks { get; set; }
+    }
+}
---
+++ TodoManager/Models/Domain/NewTask.cs
@@ -0,0 +1,13 @@
+﻿namespace TodoManager.Models.Domain
+{
+    public class NewTask
+    {
+        public Guid id { get; set; }
+        public string description { get; set; }
+        public int priority { get; set; }
+        public DateTime dueDate { get; set; }
+        public DateTime createdTime { get; set; }
+        public int label { get; set; }
+        public bool isVisible { get; set; }
+    }
+}
